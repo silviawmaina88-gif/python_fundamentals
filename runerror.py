b51d48aff9ef51281f8ff0b2798913210cfc2026
Silvia W Maina <silvia.wmaina88@gmail.com>
2025-12-04 11:45:30 +0300

    add

---
+++ runerror.py
@@ -0,0 +1,31 @@
+# zero division error
+#def calculate_average(total, count):
+   # return total / count
+
+#result = calculate_average(100, 0)
+#print(result)
+# zero division error: division by zero
+
+# type error
+#age ="25"
+#next_age= age + 1
+#print(next_age)
+#type error: can only concatenate str (not "int") to str
+
+# index error
+#numbers=[1,2,3,4,5]
+#print(numbers[10])
+#index error: list index out of range
+
+# value error
+#user_input="abc"
+#number=int(user_input)
+#value error:invalid literal for int() with base 10: 'abc'
+
+#filenotfounderror
+#with open('nonexistent.txt', 'r') as file:
+    #content=file.read()
+#filenotfounderror: [errno 2]no such file or directory: 'nonexistent.txt'
+
+
+
